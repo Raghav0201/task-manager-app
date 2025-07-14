64bf52177b8015e14945049db74d0daf1b231ba8
Raghav <raghavelumalai@gmail.com>
2025-07-14 10:33:27 +0530

    Add files via upload

--- Backend/models/Task.js
+++ Backend/models/Task.js
@@ -1,26 +1,41 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
   title: {
     type: String,
-    required: true
+    required: true,
   },
   todos: {
     type: [String],
-    default: []
+    default: [],
   },
-  dueDate: {
-    type: Date
+  description: String,
+  status: {
+    type: String,
+    enum: ['pending', 'in progress', 'completed'],
+    default: 'pending',
   },
+  dueDate: Date,
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  sharedWith: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    }
+  ],
   completed: {
     type: Boolean,
-    default: false
-  }
-});
+    default: false,
+  },
+  priorityType: {
+    type: String,
+    enum: ['early', 'late'],
+    default: 'late',
+  },
+}, { timestamps: true });
 
 module.exports = mongoose.model('Task', taskSchema);
